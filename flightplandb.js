d222bc8aec69ec326085a38ed6715d5af2fb5498
Dante Fragapane <dante.fragapane@gmail.com>
2019-06-26 11:50:03 -0400

    Made the class
    Class contains two methods, with one being one of the main methods the class will contain

---
+++ flightplandb.js
@@ -0,0 +1,24 @@
+const request = require('request')
+const baseUri = 'https://api.flightplandatabase.com'
+
+module.exports = class FlightPlanDB {
+  constructor (apiKey) {
+    this.apiKey = apiKey
+  }
+
+  getFlightPlan (id, callback) {
+    this._sendRequest('plan', id, callback)
+  }
+
+  _sendRequest (type, options, callback) {
+    const url = `${baseUri}/${type}/${options}`
+    request(url, function (err, response, body) {
+      if (err) {
+        console.error(err)
+      }
+      if (!err) {
+        callback(JSON.parse(body))
+      }
+    })
+  }
+}
